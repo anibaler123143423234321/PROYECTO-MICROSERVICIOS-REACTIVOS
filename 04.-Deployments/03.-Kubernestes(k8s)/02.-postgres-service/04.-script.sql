426294964f1a08b6c0bb189f66495f1430a4cd40
anibaler123143423234321 <dagner_smooth@outlook.com>
2026-02-19 10:20:21 -0500

    comandos docker y kubernetes

--- 04.-Deployments/03.-Kubernestes(k8s)/02.-postgres-service/04.-script.sql
+++ 04.-Deployments/03.-Kubernestes(k8s)/02.-postgres-service/04.-script.sql
@@ -1,30 +1,25 @@
-CREATE SEQUENCE seq_jobs
+CREATE SEQUENCE seq_product
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
     NO MAXVALUE
     CACHE 20;
 
-CREATE TABLE jobs (
-        job_id BIGINT NOT NULL DEFAULT nextval('seq_jobs'),
+CREATE TABLE product (
+        product_id BIGINT NOT NULL DEFAULT nextval('seq_product'),
         name         VARCHAR(60) NOT NULL,
         description  VARCHAR(400)  NOT NULL,
         state        VARCHAR(1)   NOT NULL,
 
-        CONSTRAINT pk_jobs PRIMARY KEY (job_id),
-        CONSTRAINT uq_jobs_name UNIQUE (name)
+        CONSTRAINT pk_product PRIMARY KEY (product_id),
+        CONSTRAINT uq_product_name UNIQUE (name)
 );
 
-CREATE INDEX idx_jobs_name  ON jobs(name);
+CREATE INDEX idx_product_name  ON product(name);
 
-INSERT INTO jobs (name, description, state) VALUES
-('CEO','CChief Executive Officer','1'),
-('VP','Vice President','1')
+INSERT INTO product (name, description, state) VALUES
+('Latop HP','Latop HP 15" i7','1'),
+('Mouse Gamer','Mouse Logitech G502','1')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO jobs (name, description, state) VALUES
-('ADM','General Admin','1'),
-('CIF','Execute Financial','1')
-ON CONFLICT (name) DO NOTHING;
-
-ALTER SEQUENCE seq_jobs RESTART WITH 3;
+ALTER SEQUENCE seq_product RESTART WITH 3;
